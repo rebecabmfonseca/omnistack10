166cf554cb0b709876f4a8dd47ec0ae47cf8b5c9
Rebeca <rebecabmfonseca@gmail.com>
2020-01-17 07:14:08 -0300

    final web

--- web/src/App.js
+++ web/src/App.js
@@ -1,4 +1,5 @@
-import React, {useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
+import api from './services/api';
 
 import './css/global.css'
 import './css/App.css'
@@ -16,10 +17,20 @@ import './css/Main.css'
 
 function App() {
 
+  const [latitude,setLatitude] = useState('');
+  const [longitude,setLongitude] = useState('');
+  const [github_user,setGithub_user] = useState('');
+  const [techs,setTechs] = useState('');
+  
+  const [devs,setDevs] = useState([])
+
   useEffect(() =>{
     navigator.geolocation.getCurrentPosition(
       (position)=>{
           console.log(position)
+          const { latitude, longitude } = position.coords;
+          setLatitude(latitude)
+          setLongitude(longitude)
 
       },
       (error) =>{
@@ -29,29 +40,70 @@ function App() {
       }
     )
   },[])
+
+  useEffect(() => {
+    async function carregaDevs(){
+      const response = await api.get('/devs');
+      setDevs(response.data)
+    }
+    carregaDevs()
+  },[])
+
+  async function addDev(e){
+    e.preventDefault();
+    const response = await api.post('/devs', {
+      github_user,
+      techs,
+      latitude,
+      longitude
+    })
+
+    setGithub_user('')
+    setTechs('')
+    
+    
+  }
   
 
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form>
+        <form onSubmit={addDev}> 
           <div className="input-block">
             <label htmlFor="github_user">Usuário do Github</label>
-            <input name="github_user" id="github_user" required></input>
+            <input name="github_user" 
+            id="github_user" 
+            value={github_user}
+            onChange={e => setGithub_user(e.target.value)}
+            required></input>
           </div>
           <div className="input-block">
             <label htmlFor="techs">Techs</label>
-            <input name="techs" id="techs" required></input>
+            <input name="techs" 
+            id="techs" 
+            value={techs}
+            onChange={e => setTechs(e.target.value)}
+            required></input>
           </div>
           <div className="input-group">
             <div className="input-block">
               <label htmlFor="latitude">Latitude</label>
-              <input name="latitude" id="latitude" required></input>
+              <input type="number" 
+              name="latitude" 
+              id="latitude" 
+              value={latitude} 
+              onChange={ e=> setLatitude(e.target.value)} 
+              required></input>
             </div>
             <div className="input-block">
               <label htmlFor="longitude">Longitude</label>
-              <input name="longitude" id="longitude" required></input>
+              <input type="number" 
+              name="longitude" 
+              id="longitude" 
+              value={longitude} 
+              onChange={ e=> setLongitude(e.target.value)} 
+              required></input>
             </div>
           </div>
           <button type="submit">Salvar</button>
@@ -60,65 +112,24 @@ function App() {
       </aside>
       <main>
         <ul>
-          <li className="dev-item">
+          {devs.map( dev => (
+            <li key={dev._id} className="dev-item">
             <header>
-              <img src="https://avatars3.githubusercontent.com/u/4217842?s=460&v=4" />
+              <img src={dev.avatar_url} alt={dev.name} />
               <div className="user-info">
-                <strong>Rebeca</strong>
-                <span>React1,React2,React3</span>
+                <strong>{dev.name}</strong>
+                <span>{dev.techs.join(', ')}</span>
               </div>
             </header>
 
             <p>
-              Biografia não autorizada.
+              {dev.bio}
             </p>
-            <a href="https://github.com/rebecabmfonseca">Acessar perfil no Github</a>
+            <a href={`https://github.com/${dev.github_user}`}>Acessar perfil no Github</a>
           </li>
+          ))}
+          
 
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars3.githubusercontent.com/u/4217842?s=460&v=4" />
-              <div className="user-info">
-                <strong>Rebeca</strong>
-                <span>React1,React2,React3</span>
-              </div>
-            </header>
-
-            <p>
-              Biografia não autorizada.
-            </p>
-            <a href="https://github.com/rebecabmfonseca">Acessar perfil no Github</a>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars3.githubusercontent.com/u/4217842?s=460&v=4" />
-              <div className="user-info">
-                <strong>Rebeca</strong>
-                <span>React1,React2,React3</span>
-              </div>
-            </header>
-
-            <p>
-              Biografia não autorizada.
-            </p>
-            <a href="https://github.com/rebecabmfonseca">Acessar perfil no Github</a>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars3.githubusercontent.com/u/4217842?s=460&v=4" />
-              <div className="user-info">
-                <strong>Rebeca</strong>
-                <span>React1,React2,React3</span>
-              </div>
-            </header>
-
-            <p>
-              Biografia não autorizada.
-            </p>
-            <a href="https://github.com/rebecabmfonseca">Acessar perfil no Github</a>
-          </li>
         </ul>
 
       </main>
